b40304a855bccb5228a5f342e8fb1c09197e6fd4
ivanzeng <zanyfly@126.com>
2022-08-01 18:27:10 +0800

    update the sh, gitignore

--- makeframework.sh
+++ makeframework.sh
@@ -39,12 +39,19 @@ function build_framework {
     xcodebuild -project ${project_name}.xcodeproj -sdk iphonesimulator -configuration "Release" OTHER_CFLAGS="-fembed-bitcode" BUILD_DIR="./${build_dir}" build || exit 1
 }
 
+function update_demo {
+    cp -R -f ${build_dir}/Release-iphonesimulator/${framework_name}.framework Example/Teeee/Simulator
+    cp -R -f ${build_dir}/Release-iphoneos/${framework_name}.framework Example/Teeee/Device
+}
+
+
 # 合并
 function build_fat_framework {
     echo "======build_fat_framework======"
     mkdir -p ${out_dir}
 
     cp -R ${build_dir}/Release-iphoneos/${framework_name}.framework ${out_dir}/
+    update_demo
     lipo -create ${build_dir}/Release-iphonesimulator/${framework_name}.framework/${framework_name} ${build_dir}/Release-iphoneos/${framework_name}.framework/${framework_name} -output ${out_dir}/${framework_name}.framework/${framework_name} || exit 1
 }
 
@@ -57,9 +64,12 @@ function build_zip {
     zip -r -m -o ${framework_name}.framework.zip ${framework_name}.framework || exit 1
 }
 
+
 # 调用
 build_clean
 build_config
 build_framework
 build_fat_framework
 build_zip
+
+
